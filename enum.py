177f4b0f6bf90b55a3a6a71aa9944580735e34d5
Halil Bilir <hbilir@gmail.com>
2015-12-10 23:17:19 +0200

    created incoming message wrapper object and message type enums

---
+++ enum.py
@@ -0,0 +1,10 @@
+class Enum(set):
+    def __getattr__(self, name):
+        if name in self:
+            return name
+        raise AttributeError
+
+
+class Types:
+    originTypes = Enum(["SERVER", "LOCAL"])
+    responseTypes = Enum(["NEW_LOGIN", "REJECTED", "PRIVATE_MES_FAILED", "EMPTY"])
---
+++ incoming_message.py
@@ -0,0 +1,5 @@
+class IncomingMessage:
+    type = -1
+
+    def __init__(self, origin):
+        self.origin = origin
